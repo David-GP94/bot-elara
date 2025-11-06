3bff9b5d5966b2996901781c986dc09e7b76935e
David <david_bono94@hotmail.com>
2025-11-05 23:19:24 -0600

    first commit

---
+++ src/main/java/com/bot/elara/ElaraBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.elara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElaraBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElaraBotApplication.class, args);
+	}
+
+}
